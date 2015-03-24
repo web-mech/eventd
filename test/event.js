2d5993ab705b40abf7c45566139193996e143961
webmech <webmech@gmail.com>
2015-03-24 12:04:14 -0400

    some basic tests for event and observe

---
+++ test/event.js
@@ -0,0 +1,30 @@
+var traceur = require('traceur'),
+  expect = require('chai').expect,
+  should = require('chai').should();
+  
+traceur.require.makeDefault(function(filename) {
+  // don't transpile our dependencies, just our app
+  return filename.indexOf('node_modules') === -1;
+});
+
+
+var Event = require('../lib/event.js').Event;
+
+describe('Event', function() {
+  
+  it('Can bind to a single event', function() {
+      var evt = new Event();
+      evt.on('foo', function() {});
+      should.exist(evt._events['foo']);
+      expect(evt._events['foo'].length).to.equal(1);
+  });
+
+  it('Can trigger bound events', function(done) {
+    var evt = new Event();
+    evt.on('foo', function(bar) {
+      expect(bar).to.equal('bar');
+      done();
+    });
+    evt.trigger('foo', 'bar');
+  });
+});
--- test/observe.js
+++ test/observe.js
@@ -1,4 +1,5 @@
-var traceur = require('traceur');
+var traceur = require('traceur'),
+  expect = require('chai').expect;
 
 traceur.require.makeDefault(function(filename) {
   // don't transpile our dependencies, just our app
@@ -6,13 +7,26 @@ traceur.require.makeDefault(function(filename) {
 });
 
 
-var o = require('../lib/observe.js').Observe;
-
-
+var Observe = require('../lib/observe.js').Observe;
 
 describe('Observe', function() {
-  it('is very cool', function() {
-    var m = new o({a:1});
-    console.log(m);
+  it('Has observable properties', function(done) {
+    var mammal = new Observe({
+      type: 'elephant'
+    });
+    mammal.on('type', function(type) {
+      expect(type).to.equal('horse');
+      done();
+    });
+    mammal.type = 'horse';
+  });
+
+  it('Can set new observable properties, observe them before they are set.', function(done) {
+    var human = new Observe({});
+    human.on('name', function(name) {
+      expect(name).to.equal('mike');
+      done();
+    });
+    human.set('name', 'mike');
   });
 });
