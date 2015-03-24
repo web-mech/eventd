ee3e52adc01387130faa4d80e2f825fb3d592875
webmech <webmech@gmail.com>
2015-03-24 12:22:28 -0400

    fixed build process for web use

---
+++ Gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+    $ = require('gulp-load-plugins')();
+
+gulp.task('traceur:runtime', function() {
+  return gulp.src($.traceur.RUNTIME_PATH)
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['traceur:runtime'], function() {
+  var runtimePath = $.traceur.RUNTIME_PATH,
+    filter = $.filter(['*','!traceur-runtime.js']);
+
+  return gulp.src([runtimePath, 'lib/*.js'])
+    .pipe($.order([
+      'traceur-runtime.js',
+      'event.js',
+      'observe.js',
+      'app.js'
+    ]))
+    //.pipe($.sourcemaps.init())
+    .pipe(filter)
+    .pipe($.traceur({
+      sourceMap: true,
+      modules: 'inline'
+    }))
+    .pipe(filter.restore())
+    .pipe($.concat('index.js'))
+    //.pipe($.sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'));
+});
